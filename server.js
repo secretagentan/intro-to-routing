f9b89016223679cad3871a1aec54c29a019eb2a9
an nguyen <hello.an.ng@gmail.com>
2017-01-05 14:36:24 -0800

    saves routes object

--- server.js
+++ server.js
@@ -8,20 +8,40 @@ var port = 3000;
 //   var method = request.method;
 //   var url = request.url;
 //   console.log('Incoming ' + method + ' request' + ' to ' + url);
-//   response.statusCode = 200;
-//   response.setHeader('Content-Type', 'text/plain');
+//   response.statusCode = 302;
+//   response.setHeader('Location', 'http://www.google.com');
 //   response.write('Hello');
 //   response.end();
 // });
+
+var routes = [
+  { method: 'GET', path: '/', content: 'Hello & Welcome'},
+  { method: 'GET', path: '/kittens', content: 'meow'},
+  { method: 'GET', path: '/randomNumber', content: '________'},
+  { method: 'GET', path: '/eightball', content: '________'},
+  { method: 'GET', path: '/greet/[name]', content: 'Hello ' + name + '!'},
+  { method: 'GET', path: '/coinToss', content: '________'},
+  { method: 'GET', path: '/visit?destination=paris', content: '________'},
+];
 
 var server = http.createServer(function(request, response) {
   var method = request.method;
   var url = request.url;
   console.log('Incoming ' + method + ' request' + ' to ' + url);
-  response.statusCode = 302;
-  response.setHeader('Location', 'http://www.google.com');
-  response.write('Hello');
-  response.end();
+  var action = routes.find(function(route){
+    return route.method === method && route.path === url;
+  });
+  if ( action ) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.write( action.content );
+    response.end();
+  } else {
+    response.statusCode = 404;
+    // response.setHeader('Content-Type', 'text/plain');
+    response.write('uhoh');
+    response.end();
+  }
 });
 
 server.listen(port, hostname, function(){
