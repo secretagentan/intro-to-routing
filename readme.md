4236c799c31691e317ee81e6614f44661e51835c
an nguyen <hello.an.ng@gmail.com>
2017-01-05 14:15:04 -0800

    initializes

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ When a user visits '/' display "Hello and welcome"
 
 When a user visits '/randomNumber' display a number from 1 to 10
 
-When a user visits '/eighball' display "Yes" | "No" | "Maybe"
+When a user visits '/eightball' display "Yes" | "No" | "Maybe"
 
 When a user visits '/greet/[name]' display "Hello [name]"
 for example '/greet/sam' displays "Hello Sam!"
--- server.js
+++ server.js
@@ -4,12 +4,22 @@ var fs = require('fs');
 var hostname = '127.0.0.1';
 var port = 3000;
 
+// var server = http.createServer(function(request, response) {
+//   var method = request.method;
+//   var url = request.url;
+//   console.log('Incoming ' + method + ' request' + ' to ' + url);
+//   response.statusCode = 200;
+//   response.setHeader('Content-Type', 'text/plain');
+//   response.write('Hello');
+//   response.end();
+// });
+
 var server = http.createServer(function(request, response) {
   var method = request.method;
   var url = request.url;
   console.log('Incoming ' + method + ' request' + ' to ' + url);
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
+  response.statusCode = 302;
+  response.setHeader('Location', 'http://www.google.com');
   response.write('Hello');
   response.end();
 });
